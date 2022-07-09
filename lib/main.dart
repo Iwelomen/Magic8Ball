3582b8c00ec3a959dd3ef8e1cea74716db82bb15
Iwelomen <godspowerobami@gmail.com>
2022-07-09 15:28:21 +0100

    8balls git push origin main
    q
    git status

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,18 +12,40 @@ void main() {
   );
 }
 
-class Magic8Ball extends StatelessWidget {
+class Magic8Ball extends StatefulWidget {
 
 
   @override
+  State<Magic8Ball> createState() => _Magic8BallState();
+}
+
+class _Magic8BallState extends State<Magic8Ball> {
+  int ballState = 1;
+
+  void changeBall() {
+    setState(() {
+      ballState = Random().nextInt(5) + 1;
+    });
+  }
+  @override
   Widget build(BuildContext context) {
+
     return Scaffold(
+      backgroundColor: Colors.lightBlue,
+
       appBar: AppBar(
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.blueAccent,
         title: Text('Ask Me Anything'),
       ),
-      body: Container(
-        child: Image.asset('images/ball1.png'),
+      body: Center(
+        child: Container(
+          child: OutlinedButton(
+            onPressed: () {
+              changeBall();
+            },
+              child: Image.asset('images/ball$ballState.png')),
+
+        ),
       ),
     );
   }
